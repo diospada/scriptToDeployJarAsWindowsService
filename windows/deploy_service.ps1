d7918d591ab7a903b85b389bd1e0a4e5816d36b9
Alberto Bobice <alberto.bobice@gmail.com>
2019-05-20 14:21:11 +0200

    Update deploy_service.ps1

--- windows/deploy_service.ps1
+++ windows/deploy_service.ps1
@@ -6,4 +6,13 @@ write-output "Installing windows service $serviceName"
 #create xml file necessary to install microservice as windows service
 powershell -Command "(gc deploy_service.xml) -replace '%SERVICE_NAME%', '$serviceName' -replace '%JAR_NAME%', '$jarName' | Out-File -encoding ASCII $serviceNameXml"
 #rename exe that need to invoked to install microservice as windows service
-Rename-Item -Path 'WinSW.NET4.exe' -NewName $serviceNameExe
+Rename-Item -Path 'WinSW.NET4.exe' -NewName $serviceNameExe -ErrorAction SilentlyContinue
+
+#if service is running stop it before installation
+if (Get-Process $serviceName -ErrorAction SilentlyContinue) {
+  & $serviceNamePath 'stop' 
+}
+& $serviceNamePath 'uninstall'
+& $serviceNamePath 'install'
+& $serviceNamePath 'start'
+
