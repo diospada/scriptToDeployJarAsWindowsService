6ec98f1426f0247c9e43c47f051e9faa2ef9a4c7
Alberto Bobice <alberto.bobice@gmail.com>
2019-06-06 10:49:08 +0200

    Update deploy_service.sh

--- linux/deploy_service.sh
+++ linux/deploy_service.sh
@@ -3,7 +3,7 @@
 serviceName=$1
 installationDir=$2
 jarName=$3
-cat $installationDir/template_linux.txt | sed s+%SERVICE_NAME%+$serviceName+g | sed s+%INSTALLATION_DIR%+$installationDir+g | sed s+%JAR_NAME%+$jarName+g > $serviceName.service
+cat $installationDir/deployService.txt | sed s+%SERVICE_NAME%+$serviceName+g | sed s+%INSTALLATION_DIR%+$installationDir+g | sed s+%JAR_NAME%+$jarName+g > $serviceName.service
 systemctl stop $serviceName.service
 mv $serviceName.service /etc/systemd/system
 systemctl daemon-reload
